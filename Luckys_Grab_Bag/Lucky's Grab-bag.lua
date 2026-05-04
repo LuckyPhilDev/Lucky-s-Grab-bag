a7361eb99244763cdb8f26b06866552cb1806e7b
Lucky Phil <jackclubs.dev@gmail.com>
2026-05-04 12:18:16 +0100

    chore: Remove SwapBars feature

--- Luckys_Grab_Bag/Lucky's Grab-bag.lua
+++ Luckys_Grab_Bag/Lucky's Grab-bag.lua
@@ -55,7 +55,6 @@ eventFrame:SetScript("OnEvent", function(_, event, addonLoaded)
         LuckyGrabbag.CombatPrep:Init(db)
         LuckyGrabbag.RotationGlow:Init(db)
         LuckyGrabbag.ConfirmPurchase:Init(db)
-        LuckyGrabbag.SwapBars:Init()
 
         -- Minimap button
         LuckyGrabbag.minimapButton = LuckyMinimap:Create({
